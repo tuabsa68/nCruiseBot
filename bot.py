e15ce635adb33b758b1e2c7be6e325ece0b48d61
tuabsa68 <tuabsa68@gmail.com>
2025-03-10 12:20:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,3 +38,9 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+WEBHOOK_URL = f"{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/"
+
+# Webhook'ни ўрнатиш
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL)
