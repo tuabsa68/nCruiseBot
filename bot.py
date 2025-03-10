0c646e7c7e26b64f3c346dfbf7f93d039dace8d3
tuabsa68 <tuabsa68@gmail.com>
2025-03-10 15:09:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,26 +2,23 @@ from flask import Flask, request
 import telebot
 import os
 
-API_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
-bot = telebot.TeleBot(API_TOKEN)
 
 @app.route('/', methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_str = request.get_data().decode('UTF-8')
-        update = telebot.types.Update.de_json(json_str)
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-    return 'Webhook received!', 200
+        return '', 200
+    else:
+        return '', 403
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Assalomu alaykum! Men nCruiseBot botiman. Savollaringizni yozing!")
+@app.route('/', methods=['GET'])
+def index():
+    return "Bot is running!", 200
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
